252f891c7ab0dae4f78b3015d63b05b6d24a3f16
Kevin Gutowski <kskiviolin@gmail.com>
2020-11-06 14:04:45 -0800

    Initial Commit

---
+++ BlockFormatter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlockFormatter
+//
+//  Created by Kevin Gutowski on 11/6/20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
